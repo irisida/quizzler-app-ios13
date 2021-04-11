c37860b3c504d9c065f3e1d394258b0ef75cb0ae
irisida <20445648+irisida@users.noreply.github.com>
2021-04-11 14:21:32 +0100

    readme and proj files

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
+    var quizBrain = QuizBrain()
+    
+    
+    
+    // update the UI for the quesiton showing which is
+    // directly related to the index of the array of
+    // questions that questionNumber is pointed at.
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getPlayerScore())"
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userWasCorrect = quizBrain.checkAnswer(sender.currentTitle!)
+        
+        if userWasCorrect {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Quizzler
+
+## App Details
+
+You will program a trivia quiz app. If you have another field of expertise such as law or medicine, you’ll be glad to know that multiple choice question apps are one of the most popular types of educational apps on the App Store!
